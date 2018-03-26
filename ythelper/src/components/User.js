482296736302a77c8079bccfebfd5afc27fb68da
Mikko Takala <mitami@ad.helsinki.fi>
2018-03-26 11:39:27 +0300

    Useria muokattu, kesken vielä

--- ythelper/src/components/User.js
+++ ythelper/src/components/User.js
@@ -1,11 +1,26 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
 class User extends React.Component {
     render() {
         return (
             <div>
                 <h2>Tähän käyttäjän nimi</h2>
+                <div>
+                  <h3>Playlists</h3>
+                  <Playlists />
+                </div>
             </div>
         )
     }
 }
+
+const mapStateToProps = () => {
+  
+}
+
+const mapDispatchToProps = {
+
+}
+
+export default ConnectedUser (mapStateToProps, mapDispatchToProps)(User)
